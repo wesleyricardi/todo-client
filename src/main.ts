82d6457e0a3aa3ed498c57a0b9ad53813376813c
wesleyricardi <wesley14_leandro@hotmail.com>
2023-03-24 09:40:40 -0300

    criado o html do projeto

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    hello world!
-  </div>
+  <div class="container">
+      <p>
+        <label for="new-task">Nova tarefa</label>
+        <input id="new-task" type="text">
+        <button id="add-task">Criar</button>
+      </p>
+      
+      <h3>Tarefas:</h3>
+      <ul id="tasks-container">
+      </ul>
+    </div>
 `;
+
 export {};
